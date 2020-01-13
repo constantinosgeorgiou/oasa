0d48d9c51353cfbe9247db50c7ca5e300e4a9102
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2020-01-14 01:50:38 +0200

    Add views and GET requests for signup and login

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,11 @@ const router = express.Router()
 // Root route
 router.get('/', (request, response) => response.render('pages/index'))
 
+// Register / Sign up route
+router.get('/register', (request, response) => response.render('pages/register'))
+
+// Log in route
+router.get('/login', (request, response) => response.render('pages/login'))
+
+
 module.exports = router;
