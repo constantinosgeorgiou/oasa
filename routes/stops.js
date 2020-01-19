8e1bc9ea9c2b7fd9e88339d1b96be81d312899fc
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2020-01-19 05:12:38 +0200

    ADD select stop functionality, ADD index and show pages for stops

--- routes/stops.js
+++ routes/stops.js
@@ -6,17 +6,17 @@ const {
     pool
 } = require('../migrations/config')
 
-// INDEX - show all routes
+// INDEX - show all stops
 router.get('/', (request, response) => {
-    pool.query('SELECT * FROM routes WHERE isairportroute=FALSE', (error, results) => {
+    pool.query('SELECT * FROM stops', (error, results) => {
         if (error) {
             console.log(error)
             throw error
         }
         console.log("\n\n RETRIEVED\n ---------\n\n", results.rows)
-        response.render('pages/routes/index', {
-            routes: results.rows,
-            page: 'routes'
+        response.render('pages/stops/index', {
+            stops: results.rows,
+            page: 'stops'
         })
     })
 })
@@ -37,96 +37,89 @@ router.get('/accessibility', (request, response) => {
 })
 
 
-// AIRPORT - Show all airport routes
-router.get('/airport', (request, response) => {
-    pool.query('SELECT * FROM routes WHERE isairportroute=TRUE', (error, results) => {
-        if (error) {
-            console.log(error)
-            throw error
-        }
-        console.log("\n\n RETRIEVED\n ---------\n\n", results.rows)
-        response.render('pages/routes/routesairport', {
-            routes: results.rows,
-            page: 'routes'
-        })
-    })
-})
-
-
-
-// SHOW - show more info for a specific route or all routes
+// SHOW - show more info for a specific stop or all stops
 router.post('/', (request, response) => {
     const {
-        search_route
+        search_stop
     } = request.body
-    const specific = 'SELECT * FROM routes WHERE rname=$1'
-    const all = 'SELECT * FROM routes'
-    // Decide to retrieve all routes or not
-    if (search_route === '') {
+    const specific = 'SELECT * FROM stops WHERE sname=$1'
+    const all = 'SELECT * FROM stops'
+    // Decide to retrieve all stops or not
+    if (search_stop === '') {
         pool.query(all, (error, results) => {
             if (error) {
                 console.log(error)
                 throw error
             } else {
-                response.redirect('/routes')
+                response.redirect('/stops')
             }
         })
     } else {
-        pool.query(specific, [search_route], (error, results) => {
+        pool.query(specific, [search_stop], (error, results) => {
             if (results.rowCount == 0) {
-                request.flash('warning', "Η γραμμή δεν υπάρχει");
+                request.flash('warning', "Η στάση δεν υπάρχει");
                 response.redirect('back')
             } else {
-                response.redirect('/routes/' + search_route)
+                console.log(results)
+                response.redirect('/stops/' + results.rows[0].id)
             }
         })
     }
 })
-router.get('/:route', (request, response) => {
-    const retrieveRoute = 'SELECT * FROM routes WHERE rname=$1'
-    const retrieveStops = 'SELECT sname FROM stops WHERE id=$1'
-    let rname = request.params.route
-    pool.query(retrieveRoute, [rname], (error, results) => {
-        if (error) {
-            // console.log(error)
-            throw error
+router.get('/:id', (request, response) => {
+    const retrieveStop = 'SELECT * FROM stops WHERE id=$1'
+    const retrieveRoutes = 'SELECT rname FROM routes WHERE id=$1'
+    let stopid = request.params.id
+    pool.query(retrieveStop, [stopid], (error, results) => {
+        if (results.rowCount == 0) {
+            request.flash('warning', "Η στάση δεν υπάρχει");
+            response.redirect('/stops')
+        } else {
+            response.render('pages/stops/show', {
+                stop: results.rows[0]
+            })
         }
-        // console.log(results.rows[0].stops)
-        // let stopsArray = new Array(results.rows[0].stops.length)
-        // Retrieve route stops
-        // for (let eachStop = 1; eachStop <= results.rows[0].stops.length; eachStop++) {
-        //     console.log(results.rows[0].stops)
-        //     console.log(results.rows[0].stops.pop())
 
-        // let id = results.rows[0].stops.pop()
-        // console.log(id)
-
+        // if (error) {
+        //     // console.log(error)
+        //     throw error
         // }
-        // results.rows[0].stops.forEach(eachStop => {
-        //     console.log(results.rows[0].stops)
-        //     // console.log(results.rows[0].stops[eachStop])
-        //     let id = results.rows[0].stops.pop()
-        //     console.log(id)
+        // // console.log(results.rows[0].stops)
+        // // let stopsArray = new Array(results.rows[0].stops.length)
+        // // Retrieve route stops
+        // // for (let eachStop = 1; eachStop <= results.rows[0].stops.length; eachStop++) {
+        // //     console.log(results.rows[0].stops)
+        // //     console.log(results.rows[0].stops.pop())
 
-        // });
-        // pool.query(retrieveStops, [eachStop], (error, result) => {
+        // // let id = results.rows[0].stops.pop()
+        // // console.log(id)
 
-        //     if (error) {
-        //         console.log(error)
-        //         throw error
-        //     }
-        //     console.log(result.rows[0])
-        //     // stopsArray.push(result.rows[0].sname)
-        //     // console.log("name", stopsArray[stop])
-        // })
-        // results.rows[0].stops.forEach(stop => {
-        // })
+        // // }
+        // // results.rows[0].stops.forEach(eachStop => {
+        // //     console.log(results.rows[0].stops)
+        // //     // console.log(results.rows[0].stops[eachStop])
+        // //     let id = results.rows[0].stops.pop()
+        // //     console.log(id)
+
+        // // });
+        // // pool.query(retrieveStops, [eachStop], (error, result) => {
+
+        // //     if (error) {
+        // //         console.log(error)
+        // //         throw error
+        // //     }
+        // //     console.log(result.rows[0])
+        // //     // stopsArray.push(result.rows[0].sname)
+        // //     // console.log("name", stopsArray[stop])
+        // // })
+        // // results.rows[0].stops.forEach(stop => {
+        // // })
         // console.log("\n RETRIEVED\n ---------\n\n", results.rows)
-        // console.log(stopsArray)
+        // // console.log(stopsArray)
 
-        response.render('pages/routes/show', {
-            route: results.rows[results.rowCount - 1]
-        })
+        // response.render('pages/stops/show', {
+        //     stop: results.rows[0]
+        // })
     })
 })
 
