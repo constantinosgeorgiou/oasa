7856088068d9a461fe0b302c95467c2516490f72
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2020-01-19 19:24:16 +0200

    WORK: on edit route for users

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,7 @@ router.get('/', (request, response) => {
 router.get('/edit', (request, response) => {
     if (request.session.loggedin) {
         response.render('pages/users/edit', {
-            user: request.session.user
+            user: request.session.currentUser
         })
     } else {
         request.flash('danger', 'Permission denied')
@@ -37,20 +37,38 @@ router.get('/edit', (request, response) => {
 router.put('/edit', (request, response) => {
     if (request.session.loggedin) {
         const {
+            firstName,
+            lastName,
+            telephone,
+            afm,
             email,
             password
         } = request.body
-        const query = 'UPDATE users SET email = $1, password = $2 WHERE id = $3'
-        pool.query(query, [email, password, request.session.user.id], (error, result) => {
-            if (error) {
-                console.log(error)
-                throw error
+        const checkPassword = 'SELECT * FROM users WHERE id = $1 AND password = $2'
+        const updateData = 'UPDATE users SET firstName = $1, lastName = $2, telephone = $3, afm = $4, email = $5 WHERE id = $6'
+        pool.query(checkPassword, [request.session.currentUser.id, password], (error, result) => {
+            if (result.rowCount == 0) {
+                request.flash('danger', 'Ο κωδικός που δώσατε είναι λάθος')
+                response.redirect('/account/edit');
+            } else {
+                console.log('check => ', result.rows[0])
+                pool.query(updateData, [firstName, lastName, telephone, afm, email, result.rows[0].id], (error, result) => {
+                    if (error) {
+                        console.log(error)
+                        throw error
+                    }
+                    // request.session.currentUser.firstName = firstName
+                    // request.session.currentUser.lastName = lastName
+                    // request.session.currentUser.telephone = telephone
+                    // request.session.currentUser.afm = afm
+                    // request.session.currentUser.email = email
+                    console.log('result => ',result)
+                    request.flash('success', 'User updated.')
+                    response.redirect('/account');
+                })
             }
-            request.session.user.email = email
-            console.log(result)
-            request.flash('success', 'User updated.')
-            response.redirect('/account');
         })
+        response.redirect('/account')
     } else {
         request.flash('danger', 'Permission denied')
         response.redirect('/login')
@@ -61,12 +79,13 @@ router.put('/edit', (request, response) => {
 router.delete('/', (request, response) => {
     if (request.session.loggedin) {
         const query = 'DELETE FROM users WHERE id = $1'
-        pool.query(query, [request.session.user.id], (error, results) => {
+        pool.query(query, [request.session.currentUser.id], (error, results) => {
             if (error) {
                 console.log(error)
                 throw error
             }
-            request.session.user = {}
+            delete request.session.currentUser
+            delete response.locals.currentUser
             request.session.loggedin = false
             request.flash('success', 'Deleted user')
             response.redirect("/")
